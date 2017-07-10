232bd8751bbc56710b51f63a1068937a647c7c0a
Vince <vpicone@gmail.com>
2017-07-10 13:39:57 -0500

    Change buttons to dropdown menu.

--- src/BaseHospitalSelector.js
+++ src/BaseHospitalSelector.js
@@ -1,31 +1,15 @@
 import React from 'react';
 import Paper from 'material-ui/Paper';
-import RaisedButton from 'material-ui/RaisedButton';
 import { Link } from 'react-router-dom';
 import ArrowForward from 'material-ui/svg-icons/navigation/arrow-forward';
-import { deepOrange900, cyan800, white } from 'material-ui/styles/colors';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
 
-
-const SelectorButton = (props) => {
-  const style = {
-    width: '14.66667%',
-    margin: '1%',
-    textDecoration: 'none',
-  };
-
-  return (
-    <RaisedButton
-      labelColor={white}
-      backgroundColor={props.hospitalName === props.baseHospital ? deepOrange900 : cyan800}
-      label={props.hospitalName}
-      style={style}
-      containerElement={<Link to={props.hospitalName} />}
-    />
-  );
-};
 
 const BaseHospitalSelector = (props) => {
   const style = {
+    // display: 'flex',
+    flex: 'column',
     margin: 'auto',
     width: '700px',
     padding: '10px',
@@ -34,15 +18,17 @@ const BaseHospitalSelector = (props) => {
 
   return (
     <Paper zDepth={3} style={style}>
-      {/* to do: enable closing the hospital selector*/}
-      <h2>Which hospital are you at?</h2>
-      <SelectorButton baseHospital={props.baseHospital} hospitalName="dsmc" />
-      <SelectorButton baseHospital={props.baseHospital} hospitalName="smca" />
-      <SelectorButton baseHospital={props.baseHospital} hospitalName="dcmc" />
-      <SelectorButton baseHospital={props.baseHospital} hospitalName="smcw" />
-      <SelectorButton baseHospital={props.baseHospital} hospitalName="snw" />
-      <SelectorButton baseHospital={props.baseHospital} hospitalName="smch" />
-      <br />
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <h3>Which hospital are you at?</h3>
+        <DropDownMenu value={props.baseHospital} openImmediately={!props.baseHospital}>
+          <MenuItem value="dsmc" primaryText="Dell Seton" containerElement={<Link to="dsmc" />} />
+          <MenuItem value="smca" primaryText="Main" containerElement={<Link to="smca" />} />
+          <MenuItem value="dcmc" primaryText="Dell Children's" containerElement={<Link to="dcmc" />} />
+          <MenuItem value="smcw" primaryText="Williamson" containerElement={<Link to="smcw" />} />
+          <MenuItem value="snw" primaryText="North West" containerElement={<Link to="snw" />} />
+          <MenuItem value="smch" primaryText="Hays" containerElement={<Link to="smch" />} />
+        </DropDownMenu>
+      </div>
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}><p>Pickup&nbsp;</p> <ArrowForward /> <p>&nbsp;Dropoff</p></div>
     </Paper>
   );
