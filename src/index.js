62db24c4ef7e292ba0bce3647007acad2d4cff7d
Vince <vpicone@gmail.com>
2017-08-02 02:08:13 +0000

    Remove service worker.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AppContainer from "./AppContainer";
-import registerServiceWorker from "./registerServiceWorker";
-
+import {unregister} from "./registerServiceWorker";
+//import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<AppContainer />, document.getElementById("root"));
-registerServiceWorker();
+unregister();
+//registerServiceWorker();
